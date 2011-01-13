0b46c21ff3483d24dea162f9b2c04de138ae8396
Anton Bangratz <anton.bangratz@gmail.com>
2011-01-13 20:48:46 +0100

    Deployment added

---
+++ config/deploy.rb
@@ -0,0 +1,76 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, 'ruby-1.9.2-p0'        # Or whatever env you want it to run in.
+set :application, "cf"
+set :repository,  "gitosis@sensor.twincode.net:cf.git"
+set :deploy_to, "/var/www/blood-covenant.eu/#{application}"
+ssh_options[:forward_agent] = true
+
+set :scm, :git
+set :user, :blood-cov-eu
+set :use_sudo, false
+
+set :rails_env, :production
+set :unicorn_binary, "bundle exec unicorn"
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+
+role :web, "zerberos.secure.at"                          # Your HTTP server, Apache/etc
+role :app, "zerberos.secure.at"                          # This may be the same as your `Web` server
+role :db,  "zerberos.secure.at", :primary => true # This is where Rails migrations will run
+
+namespace :deploy do
+   task :start, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+  end
+  task :stop, :roles => :app, :except => { :no_release => true } do 
+    run "#{try_sudo} kill `cat #{unicorn_pid}`"
+  end
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end 
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "rm -f #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
+    run "ln -nfs #{shared_path}/downloads #{release_path}/public/downloads"
+  end
+    task :migrate, :roles => :db, :only => { :primary => true } do
+    rake = fetch(:rake, "rake")
+    rails_env = fetch(:rails_env, "production")
+    migrate_env = fetch(:migrate_env, "")
+    migrate_target = fetch(:migrate_target, :latest)
+
+    directory = case migrate_target.to_sym
+      when :current then current_path
+      when :latest  then current_release
+      else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+      end
+
+    puts "#{migrate_target} => #{directory}"
+    run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:autoupgrade"
+  end
+end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundled_gems')
+    release_dir = File.join(current_release, 'vendor', 'bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --deployment --without=development,test"
+  end
+end
+
+after 'deploy:update_code', 'bundler:bundle_new_release', 'deploy:symlink_shared'
---
+++ config/unicorn.rb
@@ -0,0 +1,4 @@
+worker_processes 3
+working_directory "/var/www/blood-covenant.eu/cf/current"
+listen "127.0.0.1:10080"
+pid "/var/www/blood-covenant.eu/cf/current/tmp/pids/unicorn.pid"
