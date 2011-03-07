079808295859778bc8bf2e75ed841759ff359b15
Anton Bangratz <anton.bangratz@gmail.com>
2011-03-07 23:50:32 +0100

    Counter Bugfix and make Uli happy

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,8 @@
 class Character
 
-  ALT_RANK_ID = 7
-  ALT_OFFICER_RANK_ID = 3
-  ALT_RANK_IDS = [3,7]
+  ALT_RANK_ID = 8
+  ALT_OFFICER_RANK_ID = 4
+  ALT_RANK_IDS = [4,8]
   include DataMapper::Resource
 
   property :name, String, :key => true, :length => 0..150
