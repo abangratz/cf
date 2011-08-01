88c82d9a1d056030015fe53425839ea03f1f49e5
Anton Bangratz <anton.bangratz@gmail.com>
2011-08-01 14:15:09 +0200

    Unicorn uses sockets

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
 worker_processes 3
 working_directory "/var/www/rails/cf/current"
-listen "127.0.0.1:10080"
+listen "unix:/var/www/rails/cf/shared/tmp/sockets/unicorn.sock"
 pid "/var/www/rails/cf/current/tmp/pids/unicorn.pid"
